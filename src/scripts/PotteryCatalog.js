a94a8a3e74714e9844fce253eee826aad9f0b6f1
Sabra Kozlina <sjkozlina@gmail.com>
2024-11-19 12:40:57 -0700

    Added toSellOrNotToSell and usePottery functions

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,18 @@
+let sellPottery = [];
+
+export const toSellOrNotToSell = (newItem) => {
+
+    if (newItem.cracked === false) {
+        if (newItem.weight >= 6) {
+            newItem.price = 40;
+        } else {
+            newItem.price = 20;
+        }
+        sellPottery.push(newItem)
+    }
+    return newItem
+};
+
+export const usePottery = () => {
+    return sellPottery
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
 import { firePottery } from "./Kiln.js"
+import { toSellOrNotToSell, usePottery } from "./PotteryCatalog.js"
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 3, 5)
@@ -13,7 +14,7 @@ const pottery = makePottery(mug, platter, vase, bowl, pitcher);
 console.log(pottery)
 // Fire each piece of pottery in the kiln
 let mugFired = firePottery(mug, 2000);
-let platterFired = firePottery(platter, 2300);
+let platterFired = firePottery(platter, 2200);
 let vaseFired = firePottery(vase, 2100);
 let bowlFired = firePottery(bowl, 1900);
 let pitcherFired = firePottery(pitcher, 2400);
@@ -22,6 +23,14 @@ const potteryFired = firePottery(mugFired, platterFired, vaseFired, bowlFired, p
 console.log(potteryFired)
 
 // Determine which ones should be sold, and their price
+let mugReadyToSell = toSellOrNotToSell(mugFired);
+let platterReadyToSell = toSellOrNotToSell(platterFired);
+let vaseReadyToSell = toSellOrNotToSell(vaseFired);
+let bowlReadyToSell = toSellOrNotToSell(bowlFired);
+let pitcherReadyToSell = toSellOrNotToSell(pitcherFired);
+
+const potteryReadyToSell = usePottery(mugReadyToSell, platterReadyToSell, vaseReadyToSell, bowlReadyToSell, pitcherReadyToSell);
+console.log(potteryReadyToSell);
 
 
 // Invoke the component function that renders the HTML list
